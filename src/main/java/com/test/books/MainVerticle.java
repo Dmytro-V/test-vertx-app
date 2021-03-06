8a3d27f36a45be6b4c2669ab93119f20c990d32f
dmytro-v <wvaskod@gmail.com>
2021-03-06 15:12:35 +0100

    add logger

--- src/main/java/com/test/books/MainVerticle.java
+++ src/main/java/com/test/books/MainVerticle.java
@@ -3,18 +3,22 @@ package com.test.books;
 import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
   private InMemoryBooksStore store = new InMemoryBooksStore();
 
   @Override
-  public void start(Future<Void> startFuture) throws Exception {
+  public void start(Promise<Void> startPromise) throws Exception {
     Router books = Router.router(vertx);
     books.route().handler(BodyHandler.create());
 
@@ -35,17 +39,17 @@ public class MainVerticle extends AbstractVerticle {
       .requestHandler(books)
       .listen(8888, http -> {
       if (http.succeeded()) {
-        startFuture.complete();
-        System.out.println("HTTP server strated on port 8888");
+        startPromise.complete();
+        LOG.info("HTTP server strated on port 8888");
       } else {
-        startFuture.fail(http.cause());
+        startPromise.fail(http.cause());
       }
     });
   }
 
   private void registerErrorHandler(Router books) {
     books.errorHandler(500, event -> {
-      System.err.println("Failed: " + event.failure());
+      LOG.error("Failed: ", event.failure());
       event.response()
         .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
         .end(new JsonObject().put("error", event.failure().getMessage()).encode());
@@ -77,7 +81,7 @@ public class MainVerticle extends AbstractVerticle {
     books.post("/books").handler(req -> {
       //read body
       final JsonObject requestBody = req.getBodyAsJson();
-      System.out.println("Request Body: " + requestBody);
+      LOG.info("Request Body: ", requestBody);
       //store
       store.add(requestBody.mapTo(Book.class));
       //return response
